6dc2d4e5c73747e85ae1fb83134e3b9058eceb36
Zhao Cheng <zcbenz@gmail.com>
2012-04-29 20:57:41 +0800

    change default keycmp function

--- predefined.h
+++ predefined.h
@@ -20,8 +20,9 @@ struct key_t {
     }
 };
 
-inline int keycmp(const key_t &l, const key_t &r) {
-    return strcmp(l.k, r.k);
+inline int keycmp(const key_t &a, const key_t &b) {
+    int x = strlen(a.k) - strlen(b.k);
+    return x == 0 ? strcmp(a.k, b.k) : x;
 }
 
 #define OPERATOR_KEYCMP(type) \
